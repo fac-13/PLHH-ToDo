346139f2499d60c9c87d272ce26774342c6f32f9
Parissai <parissa.jamali@gmail.com>
2018-03-07 14:25:25 +0000

    add npm

--- test.js
+++ test.js
@@ -1,7 +1,15 @@
 var test = require('tape');
 var logic = require('./logic');
 
-test('Example test', function(t) {
-  t.pass();
+
+var state = [
+  { id: -3, description: 'first todo', done: false },
+  { id: -2, description: 'second todo', done: false },
+  { id: -1, description: 'third todo', done: false },
+];
+
+
+test('Testing Tape is eorking', function(t) {  
+  t.equal(1, 1, "One should equal one");
   t.end();
 });
