24966778b0e12046e76bb11d545babe39d8bef6a
Helen Zhou <helen.zhou6@gmail.com>
2018-03-07 15:05:12 +0000

    Initial commit for branch and adds first test for deleteToDo

--- test.js
+++ test.js
@@ -13,3 +13,20 @@ test('Testing Tape is eorking', function(t) {
   t.equal(1, 1, "One should equal one");
   t.end();
 });
+
+
+// TESTS FOR ADD FUNCTION
+
+
+
+// TESTS FOR DELETE FUNCTION
+
+test('Deleting an item', function(t){
+  var actual = logic.deleteTodo(state, -1);
+  var expected = [
+    { id: -3, description: 'first todo', done: false },
+    { id: -2, description: 'second todo', done: false },
+  ];
+  t.equal(actual, expected, 'Should delete from array, instead got' + actual);
+  t.end()
+});
