d90b5e07708382eaf623aa1cff572f9c78fa7bee
Lawrence Sarpong <lawrenceeks@yahoo.com>
2018-03-08 13:18:06 +0000

    Fix bug when a todo is marked as done Relates #13

--- other.js
+++ other.js
@@ -1,9 +1,17 @@
-      // document.getElementById('js-buttonDone').addEventListener('click', function(event) {
-      //     if (document.getElementById('js-buttonDone').classList.contains('marked-done')){
-      //       document.getElementById('js-buttonDone').classList.remove('marked-done');
-      //     } else {
-      //       document.getElementById('js-buttonDone').classList.add('marked-done');
-      //     }
+      // document.querySelector('.button__done').addEventListener('click', function(event) {
+      // //     if (document.getElementById('js-buttonDone').classList.contains('marked-done')){
+      // //       document.getElementById('js-buttonDone').classList.remove('marked-done');
+      // //     } else {
+      // //       document.getElementById('js-buttonDone').classList.add('marked-done');
+      // //     }
+
+      //   console.log(event);
+      // if (item.done) {
+      //   var li = document.getElementById(item.id);
+      //   console.log('li', li);
+      //   li.classList.add('marked-done');
+      // }
+
         //   event.preventDefault();
         // document.getElementById('js-buttonDone').classList.toggle('marked-done');
       // });
